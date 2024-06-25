19590e4e3c1c9bed149bdb22df9bb830c7b30f78
nicopressez <npressez@gmail.com>
2024-06-25 21:03:29 +0100

    Added auth reducer

---
+++ src/reducers/auth.ts
@@ -0,0 +1,41 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface AuthState { 
+ isLoggedIn: boolean,
+ isLoading: boolean,
+ error: string | null,
+}
+
+const initialState: AuthState = {
+ isLoggedIn: false,
+ isLoading: false,
+ error:null,
+}
+
+export const authSlice = createSlice({
+ name: "auth",
+ initialState, 
+ reducers: {
+  loginRequest(state) {
+   state.isLoading = true;
+   state.error = null;
+  },
+  loginSuccess(state) { 
+   state.isLoggedIn = true;
+   state.isLoading = false;
+   state.error = null;
+  },
+  loginFailed(state) {
+   state.error = "Invalid credentials. Please double-check your username and password.";
+   state.isLoading = false;
+  }
+ }
+})
+
+export const {
+loginRequest,
+loginSuccess,
+loginFailed
+} = authSlice.actions
+
+export default authSlice.reducer
