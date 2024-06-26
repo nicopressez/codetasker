afcd112f14031c31b8fc2234716a01941220dc3a
nicopressez <npressez@gmail.com>
2024-06-26 21:13:42 +0100

    Added Auth context and Signup logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom"
+import { AuthProvider } from "./contexts/AuthContext.jsx"
 
 function App() {
 
   return (
-    <>
+    <AuthProvider>
      <Outlet />
-    </>
+    </AuthProvider>
   )
 }
 
--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -1,4 +1,31 @@
+import { useAuth } from '../../contexts/AuthContext.jsx'
+import { useState } from 'react'
+
 export default function Signup() {
+
+ const [email, setEmail] = useState("")
+ const [password, setPassword] = useState("")
+ const [repeatPassword, setRepeatPassword] = useState("")
+
+ const { signup } = useAuth()
+
+ // Edit state values on input change
+ const handleChange = (e: React.ChangeEvent<HTMLFormElement>) => {
+  const {name, value} = e.target
+  if (name === "email") setEmail(value)
+  if (name === "password") setPassword(value)
+  if (name === "repeatPassword") setRepeatPassword(value)
+
+  return;
+ }
+
+ // Send signup request on submit
+ const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault()
+  if(password === repeatPassword) {
+   signup(email, password)
+  }
+ }
  return (
   <>
   
@@ -7,21 +34,28 @@ export default function Signup() {
      <h1 className=" font-bold">
       Signup
       </h1>
-      <form>
+      <form onSubmit={handleSubmit}>
        <input id="email" 
               type="email"
               name="email"
               placeholder="Email address"
+              value={email}
+              onChange={handleChange}
               ></input>
        <input id="password"
               type="password"
               name="password"
               placeholder="Password"
+              value={password}
+              onChange={handleChange}
               ></input>
        <input id="repeatPassword"
               type="password"
               name="repeatPassword"
-              placeholder="Repeat password"></input>
+              placeholder="Repeat password"
+              value={repeatPassword}
+              onChange={handleChange}
+              ></input>
               <input type="submit"
                      value="Submit"></input>
       </form>
