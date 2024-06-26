7881604f56d4b194b56ce1416888283c0613effe
nicopressez <npressez@gmail.com>
2024-06-26 20:05:50 +0100

    Added basic HTML for signup page

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -1,7 +1,32 @@
 export default function Signup() {
  return (
   <>
-    <h2>Le signup</h2>
+  
+
+    <div className="w-100 text-center mt-2">
+     <h1 className=" font-bold">
+      Signup
+      </h1>
+      <form>
+       <input id="email" 
+              type="email"
+              name="email"
+              placeholder="Email address"
+              ></input>
+       <input id="password"
+              type="password"
+              name="password"
+              placeholder="Password"
+              ></input>
+       <input id="repeatPassword"
+              type="password"
+              name="repeatPassword"
+              placeholder="Repeat password"></input>
+              <input type="submit"
+                     value="Submit"></input>
+      </form>
+     Already have an account? Log in
+    </div>
   </>
  )
 }
