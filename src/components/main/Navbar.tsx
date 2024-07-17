ad522d5bed27fab07480294b2ee41d07a345808f
nicopressez <npressez@gmail.com>
2024-07-17 19:47:44 +0100

    Added header menu

--- src/components/main/Navbar.tsx
+++ src/components/main/Navbar.tsx
@@ -1,6 +1,12 @@
+import { useState } from "react"
 import dotsIcon from "../../assets/dotsIcon.svg"
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+
 
 export default function Navbar() {
+
+  const [toggleMenu, setToggleMenu] = useState(false)
+
   return (
     <div className="w-full h-[13%] bg-white border-b-[1px]
     border-gray-200 flex items-center justify-between">
@@ -17,9 +23,31 @@ export default function Navbar() {
                     >
                         + Add New Task
      </button>
-     <button>
-      <img src={dotsIcon} />
-     </button>
+     <Menu as="div" className="relative font-rubik">
+      <MenuButton>
+        <img src={dotsIcon}/>
+      </MenuButton>
+      <MenuItems
+      className="absolute mt-2 mr-2 right-0 w-52 flex
+      flex-col bg-white gap-1 drop-shadow-xl rounded-lg
+        pt-2 pb-2 justify-center">
+        <MenuItem>
+          <p className="pl-6 text-gray-500 block data-[focus]:bg-gray-100">
+            Edit Board
+          </p>
+        </MenuItem>
+        <MenuItem>
+          <p className="pl-6 text-red-500 block data-[focus]:bg-gray-100">
+            Delete Board
+          </p>
+        </MenuItem>
+        <MenuItem>
+          <a className="pl-6 text-red-500 block data-[focus]:bg-gray-100" href="/license">
+            Sign out
+          </a>
+        </MenuItem>
+      </MenuItems>
+    </Menu>
      </div>
     </div>
   )
